2e9a41e1b1157bc8fffb04ac3d6b01f9cccc23b3
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2025-12-24 11:00:22 -0800

    FILE

---
+++ INFO/ALIGNMENT.sh
@@ -0,0 +1,5 @@
+# Initialize the process
+BASE_PATH="/Users/s2227120/TensorCloud/"
+
+# Run applications
+python -u "${BASE_PATH}INFO/ALIGNMENT.py" --path=$BASE_PATH 
---
+++ INFO/COSMOLOGY.sh
@@ -0,0 +1,5 @@
+# Initialize the process
+BASE_PATH="/Users/s2227120/TensorCloud/"
+
+# Run applications
+python -u "${BASE_PATH}INFO/COSMOLOGY.py" --path=$BASE_PATH
