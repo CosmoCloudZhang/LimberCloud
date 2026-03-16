1bae88ae3800d6e672f6eedf573cb8ac2dbf660a
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2026-03-16 16:31:00 -0700

    JAX/GPU/

---
+++ JAX/GPU/Y1/SINGLE.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -A m1727
+#SBATCH --nodes=1
+#SBATCH -q regular
+#SBATCH --time=04:00:00
+#SBATCH --mail-type=END
+#SBATCH --constraint=gpu
+#SBATCH -o LOG/%x_%j.out
+#SBATCH --cpus-per-task=128
+#SBATCH --ntasks-per-node=1
+#SBATCH -J JAX_GPU_Y1_SINGLE
+#SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
+
+# Load modules
+module load gpu
+module load conda
+module load cray-mpich
+module load PrgEnv-gnu
+module load cray-hdf5-parallel
+
+# Activate the conda environment
+source $HOME/.bashrc
+conda activate $CosmoENV
+
+# Set environment
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+export JAX_PLATFORMS=cuda
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Initialize the process
+TAG="Y1"
+LABEL="SINGLE"
+BASE_PATH="/pscratch/sd/y/yhzhang/LimberCloud/"
+BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/LimberCloud/"
+
+# Run applications
+python -u "${BASE_PATH}JAX/GPU/${TAG}/${LABEL}.py" --tag=$TAG --path=$BASE_PATH --label=$LABEL --folder=$BASE_FOLDER
