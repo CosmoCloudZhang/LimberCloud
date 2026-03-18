af30fd692edd94a3abe9b6f32ba987f0545c0860
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2026-03-17 18:33:46 -0700

    JAX/GPU/

---
+++ JAX/GPU/GPU.sh
@@ -0,0 +1,6 @@
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y1/SINGLE.sh &&
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y1/DOUBLE.sh &&
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y1/TRIPLE.sh &&
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y10/SINGLE.sh &&
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y10/DOUBLE.sh &&
+sbatch /pscratch/sd/y/yhzhang/LimberCloud/JAX/GPU/Y10/TRIPLE.sh
--- JAX/GPU/Y1/DOUBLE.sh
+++ JAX/GPU/Y1/DOUBLE.sh
@@ -2,13 +2,14 @@
 #SBATCH -A m1727
 #SBATCH --nodes=1
 #SBATCH -q regular
-#SBATCH --time=04:00:00
 #SBATCH --mail-type=END
+#SBATCH --time=04:00:00
 #SBATCH --constraint=gpu
-#SBATCH -o LOG/%x_%j.out
+#SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=128
 #SBATCH --ntasks-per-node=1
 #SBATCH -J JAX_GPU_Y1_DOUBLE
+#SBATCH --output=LOG/%x_%j.out
 #SBATCH --mail-user=YunHao.Zhang@ed.ac.uk
 
 # Load modules
@@ -22,21 +23,23 @@ module load cray-hdf5-parallel
 source $HOME/.bashrc
 conda activate $CosmoENV
 
-# Set environment
+# Environment variables
+export JAX_PLATFORMS=cuda
+export SLURM_CPU_BIND="cores"
+
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
 export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-export JAX_PLATFORMS=cuda
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-# Initialize the process
+# Paths
 TAG="Y1"
 LABEL="DOUBLE"
 BASE_PATH="/pscratch/sd/y/yhzhang/LimberCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/LimberCloud/"
 
-# Run applications
-python -u "${BASE_PATH}JAX/GPU/${TAG}/${LABEL}.py" --tag=$TAG --path=$BASE_PATH --label=$LABEL --folder=$BASE_FOLDER
+# Run the script
+python -u "${BASE_PATH}JAX/GPU/${TAG}/${LABEL}.py" --tag=$TAG --path=$BASE_PATH --label=$LABEL --folder=$BASE_FOLDER --number=$SLURM_CPUS_PER_TASK
