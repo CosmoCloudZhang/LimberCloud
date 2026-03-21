abeb9761ce07c1def80b93503a290cf6cef5255a
CosmoCloudZhang <YunHao.Zhang@ed.ac.uk>
2026-03-21 12:46:04 -0700

    JAX/

--- JAX/GPU/Y1/TRIPLE.sh
+++ JAX/GPU/Y1/TRIPLE.sh
@@ -25,13 +25,13 @@ conda activate $CosmoENV
 
 # Environment variables
 export JAX_PLATFORMS=cuda
-export SLURM_CPU_BIND="cores"
+export SLURM_CPU_BIND=cores
 
 export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export NUMEXPR_MAX_THREADS=$SLURM_CPUS_PER_TASK
 export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-export OMP_PLACES=threads
+export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
@@ -42,4 +42,4 @@ BASE_PATH="/pscratch/sd/y/yhzhang/LimberCloud/"
 BASE_FOLDER="/global/cfs/cdirs/lsst/groups/MCP/CosmoCloud/LimberCloud/"
 
 # Run the script
-python -u "${BASE_PATH}JAX/GPU/${TAG}/${LABEL}.py" --tag=$TAG --path=$BASE_PATH --label=$LABEL --folder=$BASE_FOLDER --number=$SLURM_CPUS_PER_TASK
+srun -n 1 -c $SLURM_CPUS_PER_TASK -G 1 python -u "${BASE_PATH}JAX/GPU/${TAG}/${LABEL}.py" --tag=$TAG --path=$BASE_PATH --label=$LABEL --folder=$BASE_FOLDER --number=$SLURM_CPUS_PER_TASK
